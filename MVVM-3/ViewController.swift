373b1255015848cc4d2a20c4fc0416d4b7330f70
Сергей Иванов <serebanium@icloud.com>
2019-07-18 09:41:25 +0300

    Initial Commit

---
+++ MVVM-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-3
+//
+//  Created by Сергей Иванов on 18/07/2019.
+//  Copyright © 2019 topMob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
